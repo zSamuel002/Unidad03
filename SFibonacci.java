00b52873626b8d2bba21a3e7abf1e41c8757ec55
zSamuel002 <143724951+zSamuel002@users.noreply.github.com>
2023-10-08 16:29:22 -0500

    sFibonacci

---
+++ SFibonacci.java
@@ -0,0 +1,35 @@
+package sfibonacci;
+import java.util.Scanner;
+/**
+ *
+ * @author Samuel Arriaga
+ */
+public class SFibonacci {
+
+    public static void main(String[] args) {
+        int num, f, f1, f2;
+        
+        f = 0;        
+        f1 = 0;
+        f2 = 1;
+
+        Scanner n = new Scanner(System.in);
+        
+        System.out.println("Serie de Fibonacci.");
+        System.out.print("Ingrese la cantidad de numeros que desea ver: ");    
+        
+        num = n.nextInt();
+        n.close();
+            
+        for (int i = 0; i < num; i++) {           
+            System.out.println(f);
+            f = f1 + f2;
+            f2 = f1;
+            f1 = f;
+
+        }
+        
+       
+    }
+    
+}
